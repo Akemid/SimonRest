f340e8097eb065b07b9c8545410666186f9d87f2
essalud <akemid@gmail.com>
2022-07-13 13:40:09 -0500

    poinstRecord app

--- SimonRest/applications/pointsRecord/serializers.py
+++ SimonRest/applications/pointsRecord/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from .models import Point
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = User
         fields = ['url', 'id', 'username', 'is_staff']
+
+class PointSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Point
+        fields = [
+            'username','level'
+        ]
